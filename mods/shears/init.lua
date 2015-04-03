6d98a39da5fd149f976af2f45d06c2d23b9df1f2
Martin Doege <mdoege@compuserve.com>
2015-04-03 13:39:24 +0200

    faster breaking of wool, leaves, etc.

--- mods/shears/init.lua
+++ mods/shears/init.lua
@@ -2,9 +2,15 @@ minetest.register_tool("shears:shears", {
     description = "Shears",
     inventory_image = "shears.png",
     tool_capabilities = {
-        max_drop_level=3,
+	stack_max = 1,
+	max_drop_level=3,
         groupcaps= {
-            crumbly={times={[1]=5.00, [2]=3.50, [3]=3.00}, uses=80, maxlevel=1}
+	        full_punch_interval = 0.5,
+	        max_drop_level=1,
+		crumbly={times={[1]=5.00, [2]=3.50, [3]=3.00}, uses=80, maxlevel=1},
+		leaves={times={[1]=0,[2]=0,[3]=0}, uses=283, maxlevel=1},
+		wool={times={[1]=0.2,[2]=0.2,[3]=0.2}, uses=283, maxlevel=1},
+		snappy={times={[1]=0.2,[2]=0.2,[3]=0.2}, uses=283, maxlevel=1},
         }
     }
 })
